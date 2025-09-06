84a2b6680dcfc4b292b7afdf3a78aeb6d9be6119
Rafael Matos <rafaelmatos@fsbtech.com>
2025-09-06 01:26:25 -0300

    first commit

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.Port || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Rafael from Node.js + ArgoCD');
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
